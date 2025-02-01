625af1f4f6775b7d4e6081da392f8dcd74560fb7
Rawa Aref <rawa_aref@hotmail.com>
2025-02-01 15:55:09 +0100

    kmom01

---
+++ js/main.js
@@ -0,0 +1,38 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+    const kmomLinks = document.querySelectorAll(".kmom-li");
+    const kmomContainers = document.querySelectorAll(".kmom-text-container");
+
+  
+
+    kmomContainers.forEach(div => {
+        div.classList.add("hide-kmom");
+    });
+
+    kmomLinks.forEach(link => {
+        link.addEventListener("click", () => {
+            const targetId = link.dataset.target;
+            const targetDiv = document.getElementById(targetId);
+     
+            const arrowSpan = link.querySelector(".arrow");
+
+
+            if (targetDiv.classList.contains("show-kmom")) {
+                targetDiv.classList.remove("show-kmom");
+                targetDiv.classList.add("hide-kmom");
+                arrowSpan.textContent = "▲";
+            } else {
+
+                targetDiv.classList.remove("hide-kmom");
+                targetDiv.classList.add("show-kmom");
+                arrowSpan.textContent = "▼";
+                targetDiv.scrollIntoView({
+                    behavior: "smooth",
+                    block: "start"
+                });
+
+            }
+        });
+    });
+});
+
